d1319bee8b6258b94f5f9aeb6f5316b5e8dd8abc
wjr21 <wjr21@sussex.ac.uk>
2023-12-13 14:52:36 +0000

    More debugging

--- gridding/gridder.py
+++ gridding/gridder.py
@@ -441,7 +441,9 @@ class GridGenerator:
             # Add the lower padded region if we aren't at the bow boundary
             if slice_ind > 0:
                 low_ind = slice_ind - self.pad_region
-                print(low_ind, slice_ind, pad_low.shape)
+                print(
+                    low_ind, slice_ind, pad_low.shape, slice_mid.shape, grid_slice.shape
+                )
                 dset[low_ind : low_ind + pad_low.shape[0], :, :] += pad_low
             else:
                 # Add in the wrapped pad cells from the first slice
