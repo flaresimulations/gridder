dd737a92964823fb836ee81cc05b72306ad902fb
wjr21 <wjr21@sussex.ac.uk>
2023-12-14 01:11:52 +0000

    Visualise grid update

--- analysis/visualise_grid.py
+++ analysis/visualise_grid.py
@@ -1,45 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-import sys
-from flare import plt as flareplt
+import argparse
 
-# Get the commandline argument for which snapshot
-num = int(sys.argv[1])
 
-# Get the simulation "tag"
-sim_tag = sys.argv[2]
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="Get a 3D mass histogram of a SWIFT simulation."
+)
+parser.add_argument(
+    "--grid",
+    type=str,
+    help="The grid file to plot",
+)
+parser.add_argument(
+    "--output",
+    type=str,
+    help="The file path for the plot",
+)
+parser.add_argument(
+    "--smoothed",
+    type=float,
+    help="Are we plotting a mass grid or smoothed overdensity?",
+    default=False,
+)
+parser.add_argument(
+    "--zwidth",
+    type=float,
+    help="The distance to project along the z axis in Mpc.",
+    default=25,
+)
+args = parser.parse_args()
 
-# Get the simulation "type"
-sim_type = sys.argv[3]
-
-# Extract the snapshot string
-snaps = [str(i).zfill(4) for i in range(0, 21)]
-snap = snaps[num]
-
-# Define path to file
-metafile = "overdensity_" + sim_tag + "_" + sim_type + "_snap%s.hdf5" % snap
-path = "/cosma7/data/dp004/FLARES/FLARES-2/Parent/" \
-       "overdensity_gridding/" + sim_tag + "/" \
-       + sim_type + "/snap_" + snap + "/" + metafile
+# Get the commandline arguments
+filepath = args.grid
+outpath = args.output
+smoothed = args.smoothed
+zwidth = args.zwidth
 
 # Open file
-hdf = h5py.File(path, "r")
+hdf = h5py.File(filepath, "r")
 
-parent = hdf["Parent"]
-boxsize = parent.attrs["Boxsize"]
+if smoothed:
+    print("Not done yet")
+else:
+    parent = hdf["Parent"]
+    boxsize = parent.attrs["Boxsize"]
 
-grid_cell_width = hdf["Delta_grid"].attrs["Cell_Width"]
-grid_cell_vol = grid_cell_width ** 3
+    grid_cell_width = hdf["Grid"].attrs["Cell_Width"]
+    grid_cell_vol = grid_cell_width**3
 
-# Compute actual kernel width
-cells_per_kernel = np.int32(np.ceil(25 / grid_cell_width[0]))
-kernel_width = cells_per_kernel * grid_cell_width
+    # Compute actual kernel width
+    cells_per_kernel = np.int32(np.ceil(zwidth / grid_cell_width[0]))
+    kernel_width = cells_per_kernel * grid_cell_width
 
-# Get grid
-grid = np.sum(hdf["Parent_Grid"][:, :, 0:cells_per_kernel], axis=-1)
-log_grid = np.zeros(grid.shape)
-log_grid[grid > 0] = np.log10(grid[grid > 0])
+    # Get grid
+    grid = np.sum(hdf["Parent_Grid"][:, :, 0:cells_per_kernel], axis=-1)
+    log_grid = np.zeros(grid.shape)
+    log_grid[grid > 0] = np.log10(grid[grid > 0])
 
 fig = plt.figure(dpi=300)
 ax = fig.add_subplot(111)
@@ -52,8 +70,7 @@ cbar.set_label("$(1 + \delta)$")
 ax.set_xlabel("$x / [\mathrm{cMpc}]$")
 ax.set_ylabel("$y / [\mathrm{cMpc}]$")
 
-fig.savefig("plots/overdensity_gird_" + sim_tag + "_" + sim_type + "_" + snap + ".png",
-            bbox_inches="tight")
+fig.savefig(outpath, bbox_inches="tight")
 
 plt.close()
 
@@ -68,7 +85,9 @@ cbar.set_label("$\log_{10}(1 + \delta)$")
 ax.set_xlabel("$x / [\mathrm{cMpc}]$")
 ax.set_ylabel("$y / [\mathrm{cMpc}]$")
 
-fig.savefig("plots/log_overdensity_loggrid_" + sim_tag + "_" + sim_type + "_" + snap + ".png",
-            bbox_inches="tight")
+fig.savefig(
+    f"{'/'.join(outpath.split(".")[-1])}_log.png",
+    bbox_inches="tight",
+)
 
 plt.close()
