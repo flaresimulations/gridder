23af295954335213a20c1cd7e37cafa9b24e898d
wjr21 <wjr21@sussex.ac.uk>
2023-12-18 17:54:04 +0000

    No you don't...

--- smoothing/grid_smoother.py
+++ smoothing/grid_smoother.py
@@ -328,20 +328,21 @@ class GridSmoother:
             # Get the i j k coordinates
             i, j, k = self.get_cell_ijk(cid)
 
+            # Account for the padding
+            i += self.pad_cells - x_offset
+            j += self.pad_cells
+            k += self.pad_cells
+
             # Calculate the centre relative to the grid
             centre = np.array(
-                [
-                    i - x_offset + self.pad_cells,
-                    j + self.pad_cells,
-                    k + self.pad_cells,
-                ],
+                [i, j, k],
                 dtype=int,
             )
 
             # Get the slice indices of the mass array we need to
-            low_i = centre[0] - 2 * self.pad_cells
-            low_j = centre[1] - 2 * self.pad_cells
-            low_k = centre[2] - 2 * self.pad_cells
+            low_i = centre[0] - self.pad_cells
+            low_j = centre[1] - self.pad_cells
+            low_k = centre[2] - self.pad_cells
 
             print(i, j, k, low_i, low_j, low_k, self.cells_per_kernel, self.pad_cells)
 
