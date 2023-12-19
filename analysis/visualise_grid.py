2fc5b43492429e61829d2c30c39aa23a2785f5ce
wjr21 <wjr21@sussex.ac.uk>
2023-12-19 16:43:06 +0000

    Adapting plotting for single stage method

--- analysis/visualise_grid.py
+++ analysis/visualise_grid.py
@@ -19,12 +19,6 @@ parser.add_argument(
     help="The file path for the plot",
 )
 parser.add_argument(
-    "--smoothed",
-    type=float,
-    help="Are we plotting a mass grid or smoothed overdensity?",
-    default=False,
-)
-parser.add_argument(
     "--zwidth",
     type=float,
     help="The distance to project along the z axis in Mpc.",
@@ -41,23 +35,22 @@ zwidth = args.zwidth
 # Open file
 hdf = h5py.File(filepath, "r")
 
-if smoothed:
-    print("Not done yet")
-else:
-    parent = hdf["Parent"]
-    boxsize = parent.attrs["Boxsize"]
+parent = hdf["Parent"]
+boxsize = parent.attrs["Boxsize"]
 
-    grid_cell_width = hdf["Grid"].attrs["CellWidth"]
-    grid_cell_vol = grid_cell_width**3
+grid_cell_width = hdf["Grid"].attrs["CellWidth"]
+grid_cell_vol = grid_cell_width**3
 
-    # Compute actual kernel width
-    cells_per_kernel = np.int32(np.ceil(zwidth / grid_cell_width[0]))
-    kernel_width = cells_per_kernel * grid_cell_width
+# Compute actual kernel width
+cells_per_kernel = np.int32(np.ceil(zwidth / grid_cell_width[0]))
+kernel_width = cells_per_kernel * grid_cell_width
 
-    # Get grid
-    grid = np.sum(hdf["MassGrid"][:, :, 0:cells_per_kernel], axis=-1)
-    log_grid = np.zeros(grid.shape)
-    log_grid[grid > 0] = np.log10(grid[grid > 0])
+# Get grid
+grid = np.sum(hdf["OverDensity"][:, :, 0:cells_per_kernel], axis=-1)
+log_grid = np.zeros(grid.shape)
+log_grid[grid > 0] = np.log10(grid[grid > 0])
+
+hdf.close()
 
 fig = plt.figure(dpi=300)
 ax = fig.add_subplot(111)
