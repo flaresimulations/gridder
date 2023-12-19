afb91b90da22f6cf5d31fc9c4133edadc67d915a
wjr21 <wjr21@sussex.ac.uk>
2023-12-19 17:11:25 +0000

    Debugging

--- region_smoother.py
+++ region_smoother.py
@@ -361,6 +361,7 @@ class RegionGenerator:
 
         # Calculate 1 + delta for each grid point and store it
         for ind, part_query in enumerate(part_queries):
+            print(part_query)
             mass = np.sum(self.part_masses[part_query])
             print(mass)
             grid[ind] = (mass / self.kernel_vol) / self.mean_density
