a93e4befa1c7449b785bc7a31540dda0f58ef56d
wjr21 <wjr21@sussex.ac.uk>
2024-05-15 14:33:08 +0100

    Removed segfaulting pragma

--- src/cell.hpp
+++ src/cell.hpp
@@ -540,7 +540,6 @@ void assignPartsAndPointsToCells(std::vector<std::shared_ptr<Cell>> &cells) {
 
   // Loop over cells attaching particles and grid points
   size_t total_part_count = 0;
-#pragma omp parallel for
   for (int cid = 0; cid < metadata.nr_cells; cid++) {
 
     // Get the cell
