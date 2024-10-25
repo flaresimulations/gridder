925db9eb1e015cb245f5fff926442a0da85c889f
wjr21 <wjr21@sussex.ac.uk>
2024-10-25 15:22:07 +0100

    Spoof MPI

--- src/gridder.cpp
+++ src/gridder.cpp
@@ -38,10 +38,14 @@ int main(int argc, char *argv[]) {
   omp_set_num_threads(nthreads);
 
   // Set up the MPI environment
-  MPI_Init(&argc, &argv);
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  // MPI_Init(&argc, &argv);
+  // int rank, size;
+  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  // TODO: Remove this when we have MPI
+  int rank = 0;
+  int size = 1;
 
   if (rank == 0) {
     say_hello();
