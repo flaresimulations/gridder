2a3a2c7cab6ecf1bc64cb3df92c23a5bd0faed00
wjr21 <wjr21@sussex.ac.uk>
2024-11-04 17:15:37 +0000

    Removing MEGA licence

--- src/serial_io.hpp
+++ src/serial_io.hpp
@@ -1,23 +1,3 @@
-/*******************************************************************************
- * This file is part of MEGA++.
- * Copyright (c) 2023 Will Roper (w.roper@sussex.ac.uk)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * This header file contains the prototypes for reading from and writing to
- * a HDF5 file in serial.
- ******************************************************************************/
 #ifndef SERIAL_IO_H_
 #define SERIAL_IO_H_
 
